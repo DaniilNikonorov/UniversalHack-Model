ed652d121f605c6e7709261f575670ce0b389151
MeinRespect <agishev1961@gmail.com>
2023-09-07 20:14:12 +0300

    commented some parts to long wait

--- main.py
+++ main.py
@@ -14,21 +14,19 @@ def classic_model_preds_get(classic_preds_table):
     interactions_preds = pd.DataFrame(data=interactions_preds.result_rows, columns=interactions_preds.column_names)
     return interactions_preds
 
-def interactions_data_get(interaction_table):
-    client = clickhouse_connect.get_client(host='94.139.255.66', username='danya', password='qwerty')
-
-    interactions = client.query(f"SELECT * FROM {interaction_table}")
-
-    interactions = pd.DataFrame(data=interactions.result_rows, columns=interactions.column_names)
-    return interactions
-
-
+# def interactions_data_get(interaction_table):
+#     client = clickhouse_connect.get_client(host='94.139.255.66', username='danya', password='qwerty')
+#
+#     interactions = client.query(f"SELECT * FROM {interaction_table}")
+#
+#     interactions = pd.DataFrame(data=interactions.result_rows, columns=interactions.column_names)
+#     return interactions
 
 global interactions_preds
 interactions_preds = classic_model_preds_get("preds_dataset")
 
-global interactions
-interactions = interactions_data_get("interactions_processed")
+# global interactions
+# interactions = interactions_data_get("interactions_processed")
 
 global dict_names_items
 with open(f"id_title_dict.pkl", 'rb') as f:
@@ -46,16 +44,16 @@ def predict_for_user(user_id):
             result_preds[value] = dict_names_items[value]
         return result_preds
 
-def get_filmes_user(user_id):
-    user_films = interactions_preds.loc[interactions['user_id'] == user_id]['item_id']
-    if len(user_films ) == 0:
-        return [(1, "No films found for user")]
-    else:
-        result_preds = {}
-        for value in preds.sample(5).values:
-            value = int(value)
-            result_preds[value] = dict_names_items[value]
-        return result_preds
+# def get_filmes_user(user_id):
+#     user_films = interactions_preds.loc[interactions['user_id'] == user_id]['item_id']
+#     if len(user_films ) == 0:
+#         return [(1, "No films found for user")]
+#     else:
+#         result_preds = {}
+#         for value in user_films.sample(5).values:
+#             value = int(value)
+#             result_preds[value] = dict_names_items[value]
+#         return result_preds
 
 @app.route("/predict_user", methods=['POST'])
 def serve_predictions():
@@ -71,19 +69,19 @@ def serve_predictions():
     except Exception as e:
         return jsonify({"error": str(e)})
 
-@app.route("/get_user_watched_filmes", methods=['POST'])
-def serve_user_filmes():
-    try:
-        # Parse the user ID from the request
-        user_id = int(request.json['user_id'])
-
-        # Get recommendations for the user
-        recommendations = get_filmes_user(user_id)
-
-        # Return the recommendations as a JSON response
-        return jsonify(recommendations)
-    except Exception as e:
-        return jsonify({"error": str(e)})
+# @app.route("/get_user_watched_filmes", methods=['POST'])
+# def serve_user_filmes():
+#     try:
+#         # Parse the user ID from the request
+#         user_id = int(request.json['user_id'])
+#
+#         # Get recommendations for the user
+#         recommendations = get_filmes_user(user_id)
+#
+#         # Return the recommendations as a JSON response
+#         return jsonify(recommendations)
+#     except Exception as e:
+#         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run()
