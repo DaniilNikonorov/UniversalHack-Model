29e3d34b14e319075242944c428df343efb92653
MeinRespect <agishev1961@gmail.com>
2023-09-07 20:08:32 +0300

    maded working predicting system

---
+++ flask_tester.py
@@ -0,0 +1,20 @@
+import requests
+
+# Define the URL of your Flask app
+app_url = 'http://127.0.0.1:5000'  # Assuming your Flask app is running locally on port 5000
+
+# Define the user ID for which you want recommendations
+user_id = 2
+
+# Create a JSON payload with the user ID
+payload = {'user_id': user_id}
+
+# Send a POST request to the /predict_user endpoint
+response = requests.post(f'{app_url}/predict_user', json=payload)
+
+# Check if the request was successful
+if response.status_code == 200:
+    recommendations = response.json()
+    print(recommendations)
+else:
+    print(f'Error: {response.status_code} - {response.text}')
