d52d39f08e9e15fde8680851c8489923354fc0d7
MeinRespect <agishev1961@gmail.com>
2023-09-09 12:56:55 +0300

    Maded simple flask

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ from models import *
 import catboost
 import lightfm
 import pickle
+import numpy as np
 
 app = Flask(__name__)
 prediction_transformer = False
 
-# def classic_model_preds_get(classic_preds_table):
-#     client = clickhouse_connect.get_client(host='94.139.255.66', username='danya', password='qwerty')
-#
-#     interactions_preds = client.query(f"SELECT * FROM {classic_preds_table}")
-#
-#     interactions_preds = pd.DataFrame(data=interactions_preds.result_rows, columns=interactions_preds.column_names)
-#     return interactions_preds
+def get_items_names(items_table):
+    client = clickhouse_connect.get_client(host='94.139.255.66', username='danya', password='qwerty')
+
+    items_names = client.query(f"SELECT * FROM {items_table}")
+
+    items_names = pd.DataFrame(data=items_names.result_rows, columns=items_names.column_names)[['item_id', 'name']]
+    return items_names
 
 def load_models():
 
     with open(f"models/popular_model_supermarket_final.pickle", 'rb') as f:
         popular_top_n = pickle.load(f)
         popular_model = popular_top_n
-
     return popular_model
 
 # def finetune_popular(items, popular_model):
@@ -45,23 +45,47 @@ def load_models():
 global popular_model
 popular_model = load_models()
 
+global item_names
+item_names = get_items_names("items_supermarket")
+
 def predict_for_user(user_id, device_id, sequence, recs_number=3):
-    return list(popular_model['device_id'].keys())[:recs_number]
+    return list(popular_model[device_id].keys())[:recs_number]
+
+def get_item_name(item_id):
+    name = item_names.loc[item_names['item_id'] == item_id, 'name']
+    if len(name) > 0:
+        return name.values[0]
+    else:
+        return "None"
+
 
 @app.route("/predict_user", methods=['POST'])
 def serve_predictions():
 
     # Parse the user ID from the request
-
     user_id, device_id, sequence = int(request.json['user_id']), \
         int(request.json['device_id']), \
         list(request.json['items_id'])
 
     # Get recommendations for the user
-    recommendations = predict_for_user(user_id, device_id, sequence)
+    recommendations = predict_for_user(user_id, device_id, sequence, recs_number=3)
     # Return the recommendations as a JSON response
+
+    recommendations = [int(x) for x in recommendations]
     return jsonify(recommendations)
 
+@app.route("/item_name", methods=['POST'])
+def get_items():
+
+    # Parse the user ID from the request
+    item_ids = list(request.json['item_ids'])
+
+    # Get recommendations for the user
+    results = []
+    for id in item_ids:
+        results.append(get_item_name(id))
+
+    return jsonify(results)
 
 if __name__ == "__main__":
     app.run(port=5001)
